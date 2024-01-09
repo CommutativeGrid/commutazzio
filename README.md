1fcab4888addb67dba6c01ffc435bee91c361b17
Ace Taffy <xucgphi@gmail.com>
2024-01-09 22:55:15 +0900

    update precompute docs and code

--- README.md
+++ README.md
@@ -11,12 +11,32 @@ This package is compatible with Python 3.10 and may not work correctly with earl
 
    Visit the [fzzpy GitHub repository](https://github.com/CommutativeGrids/fzzpy) and follow the instructions provided there to install `fzzpy`.
 
-2. **Install Commutazzio:**
+2. **Install commutazzio:**
 
-   After installing `fzzpy`, you can install Commutazzio using pip. Run the following command in your terminal:
+   After installing `fzzpy`, `commutazzio` can be installed using pip. Run the following command in your terminal:
    ```bash
    pip install --user .
    ```
+
+### Using Precomputed Intervals 
+
+For faster and more efficient computations, consider using precomputed intervals data. This approach can reduce the time spent on calculations. Here's how you can set it up:
+
+First, navigate to the `./examples/` directory and execute the following command:
+
+```bash
+python precompute_intervals.py --m_threshold=80
+```
+
+In this command, `m_threshold` represents the maximum ladder length you're considering. Executing this will generate a `./precomputed_results/` directory right where `precompute_intervals.py` is situated.
+
+Next, you'll need to update the `precomputed_intv_dir` parameter in the `config.ini` file, which is located in the root directory of `commutazzio`. This can be done by executing the command below:
+
+```bash
+python config_setup.py set_precomputed_intv_dir "./precomputed_results"
+```
+
+This simple setup process equips `commutazzio` with the necessary precomputed data, paving the way 
 
 ## License
 
--- examples/config_setup.py
+++ examples/config_setup.py
@@ -1,9 +1,32 @@
 import configparser
 from pathlib import Path
 import fire
+import importlib.util
+
+def get_package_path(package_name):
+    """
+    Get the path of a package without importing it.
+
+    Args:
+    - package_name (str): The name of the package.
+
+    Returns:
+    - Path: The path to the package directory.
+    """
+
+    # Find the spec of the package
+    package_spec = importlib.util.find_spec(package_name)
+
+    if package_spec is None:
+        raise ImportError(f"Package {package_name} not found")
+
+    # The package path is the parent directory of the __init__.py file
+    package_path = Path(package_spec.origin).parent
+
+    return package_path
 
 class CommutazzioConfig:
-
+    
     @staticmethod
     def set_precomputed_intv_dir(directory: str):
         """
@@ -15,11 +38,13 @@ class CommutazzioConfig:
         
         # Ensure the provided path is absolute
         if not Path(directory).is_absolute():
-            raise ValueError("Please provide an absolute path for the directory.")
+            directory = Path(directory).absolute()
+            # raise ValueError("Please provide an absolute path for the directory.")
         
-        # Get the location of the config.ini within the package
-        current_dir = Path(__file__).parent
-        config_path = current_dir / 'config.ini'
+        # Get the location of the config.ini in the package commutazzio
+        
+        package_dir = get_package_path("commutazzio")
+        config_path = package_dir / 'config.ini'
         
         # If config.ini doesn't exist, create it
         if not config_path.exists():
@@ -32,11 +57,13 @@ class CommutazzioConfig:
         config.read(config_path)
     
         # Set the directory
-        config['storage']['precomputed_intv_dir'] = directory
+        config['storage']['precomputed_intv_dir'] = f"'{directory}'"
     
         # Write the updated config
         with config_path.open('w') as configfile:
             config.write(configfile)
+
+        print(f"config_path:{config_path}")
     
         print(f"precomputed_intv_dir updated to: {directory}")
 

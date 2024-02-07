9d100e797bb2528381c1a0037885ddea3b39d59a
Ace Taffy <xucgphi@gmail.com>
2024-02-07 17:22:53 +0900

    add doc for cl database class

--- commutazzio/filtration/database.py
+++ commutazzio/filtration/database.py
@@ -11,12 +11,39 @@ from functools import cache
 import gc
 
 class CLFiltrationDB:
+    """
+    A class for creating and managing a SQLite database to store and retrieve 
+    instances of CLFiltration.
+    
+    Parameters
+    ----------
+    filename : str, optional
+        The name of the database file to connect to. If ':memory:', creates a 
+        temporary in-memory database. Defaults to 'clf_database.db'.
+    table_name : str, optional
+        The name of the table within the database to create or use. 
+        Defaults to 'filtration'.
+    create_new_db : bool, optional
+        If True, creates a new database file if one does not exist. Raises an 
+        exception if the file already exists. Defaults to False.
+    
+    Attributes
+    ----------
+    conn : sqlite3.Connection
+        The connection object to the database.
+    filename : str
+        The name of the database file.
+    table_name : str
+        The name of the table used for storing filtration data.
+    
+    Raises
+    ------
+    Exception
+        If `create_new_db` is True and the database file already exists, or if
+        the database file does not exist when `create_new_db` is False.
+        
+    """
     def __init__(self, filename:str='clf_database.db', table_name:str='filtration',create_new_db:bool=False):
-        """
-        initializes the database by connecting to the database file, 
-        and calls create_table() method which creates a table 
-        if it doesn't exist in the database.
-        """
         if filename == ':memory:':
             self.conn = sqlite3.connect(':memory:')
             print('Connected to in-memory database.')
@@ -44,24 +71,38 @@ class CLFiltrationDB:
                 raise Exception(f"{self.filename} does not exist.")
             
     def reset_table_name_for_legacy(self):
+        """
+        Resets the table name to the first table found in the database.
+        
+        This method is intended for legacy support and updates the instance's
+        table name attribute to match the first table name found in the
+        database schema.
+        """
         c=self.conn.cursor()
         c.execute("SELECT name FROM sqlite_master WHERE type='table';")
         tables = c.fetchall()
         self.table_name = tables[0][0]
             
     def close(self):
+        """
+        Closes the database connection.
+        """
         self.conn.close()
         print(f"Closed {self.filename} database.")
 
     def create_table(self,table_name:str):
         """
-        creates a table named table_name 
-        with columns for 
-        id, 
-        ladder_length, 
-        upper, lower, 
-        horizontal_parameters, 
-        and info.
+        Creates a table in the database with a specified name if it doesn't exist.
+        
+        Parameters
+        ----------
+        table_name : str
+            The name of the table to be created.
+        
+        Notes
+        -----
+        The table will have columns for id (primary key), ladder_length, upper, 
+        lower, horizontal_parameters, and info (stored in JSON format).
         """
         # print information if find the table already exists
         assert isinstance(table_name,str)
@@ -79,10 +120,20 @@ class CLFiltrationDB:
                                 horizontal_parameters TEXT,
                                 info JSON1)''')
 
-    def add_filtration(self, clfiltration, store_minimal_data=False):
-        # Serialize the filtration object
-        serialized_filtration = clfiltration.serialize()
+    def add_filtration(self, clfiltration:CLFiltration, store_minimal_data:bool=False):
+        """
+        Adds a CLFiltration instance to the database.
         
+        Parameters
+        ----------
+        clfiltration : CLFiltration
+            The CLFiltration instance to store in the database.
+        store_minimal_data : bool, optional
+            If True, only stores the ladder_length and info of the CLFiltration 
+            instance. Defaults to False.
+        """
+        serialized_filtration = clfiltration.serialize() # Serialize the filtration object
+    
         if store_minimal_data:
             self.conn.execute(f'''INSERT INTO {self.table_name} 
                               (ladder_length, info) VALUES (?, ?)''',
@@ -103,8 +154,21 @@ class CLFiltrationDB:
         # Save the changes
         self.conn.commit()
     
-    def get_filtration_by_id(self, id):
-        # Retrieve a row from the table by ID
+    def get_filtration_by_id(self, id:int):
+        """
+        Retrieves a CLFiltration instance from the database by its ID.
+        
+        Parameters
+        ----------
+        id : int
+            The ID of the CLFiltration instance to retrieve.
+        
+        Returns
+        -------
+        CLFiltration or None
+            The CLFiltration instance corresponding to the given ID, or None if 
+            no instance is found.
+        """
         cursor = self.conn.cursor()
         cursor.execute(f'''SELECT * FROM {self.table_name} WHERE id = ?''', (id,))
         row = cursor.fetchone()
@@ -129,12 +193,28 @@ class CLFiltrationDB:
     
     @cache
     def __len__(self):
+        """
+        Returns the number of filtration records in the table.
+        
+        Returns
+        -------
+        int
+            The number of records in the database table.
+        """
         cursor = self.conn.cursor()
         cursor.execute(f'''SELECT count(*) FROM {self.table_name}''')
         return cursor.fetchone()[0]
         
     def get_all(self):
-        # Retrieve all rows from the table
+        """
+        Retrieves all CLFiltration instances from the database.
+        
+        Returns
+        -------
+        generator of CLFiltration
+            A generator that yields CLFiltration instances for each record in 
+            the database table.
+        """
         cursor = self.conn.cursor()
         cursor.execute(f'''SELECT * FROM {self.table_name}''')
         rows = cursor.fetchall()
